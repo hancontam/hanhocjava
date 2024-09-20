4890568cd01177be8f89526af9ca44a0ca62b324
tamcon <hancontam@gmail.com>
2024-09-21 01:30:23 +0700

    init project

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world with java han ga");
+        System.out.println("Hello world with java han ga");
+        System.out.println("Hello world with java han ga");
+        System.out.println("Hello world with java han ga");
+        System.out.println("Hello world with java han ga");
+
+    }
+}
