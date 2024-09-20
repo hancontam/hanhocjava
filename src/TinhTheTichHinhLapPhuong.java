8d21be28b073232021a9f16457a01ff9eb1ebbc3
tamcon <hancontam@gmail.com>
2024-09-21 02:33:55 +0700

    tinh v hinh lap phuong

---
+++ src/TinhTheTichHinhLapPhuong.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class TinhTheTichHinhLapPhuong {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap canh hinh lap phuong di:");
+        int a = sc.nextInt();
+        System.out.println("------ket qua------");
+        System.out.println("The tich canh lap phuong la: " + Math.pow(a, 3));
+    }
+}
